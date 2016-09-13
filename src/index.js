d3503c4de2e7f1a4bfd1f1d19b1e6ba8a26271aa
Josh Burgess <joshburgess.webdev@gmail.com>
2016-09-13 12:11:00 -0400

    change linting strategy

--- src/index.js
+++ src/index.js
@@ -32,8 +32,15 @@ const initialState = 0
 const state$ = scan(reducer, initialState, actions$)
 const vTree$ = map(view, state$)
 
+// NOTE: Effectful code must always disable fp/no-unused-expression
+// This is fine. Use the linter to help you be vigilant.
+
+/* eslint-disable fp/no-unused-expression */
+
 // Logging
 enableLogging(state$)
 
 // Run app
 run(renderChanges(vTree$), document.getElementById('root'))
+
+/* eslint-enable fp/no-unused-expression */
