48deb902a94eaccab74df27ec7b9d45189956ed3
joshburgess <joshburgess.webdev@gmail.com>
2016-10-08 16:40:21 -0400

    add unchanged state return to reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,6 +4,7 @@ import Actions from '../actions'
 const reducer = (state, action) => Actions.case({
   Increment: _ => state + 1,
   Decrement: _ => state - 1,
+  _: _ => state,
 }, action)
 
 export default reducer
