6576df98fb5320eb710f16163c82404367c9a2ca
joshburgess <joshburgess.webdev@gmail.com>
2017-03-04 14:11:48 -0500

    Delete brs from Counter jsx component

--- src/components/jsx/Counter.js
+++ src/components/jsx/Counter.js
@@ -11,10 +11,6 @@ const resetStyle = {
 const Counter = ({ title, subtitle, count }) =>
   <div>
     <Button text='Reset' clickHandler={reset} style={resetStyle} />
-    <br />
-    <br />
-    <br />
-    <br />
     <Button text='-' clickHandler={decrement} />
     <h1>{count}</h1>
     <Button text='+' clickHandler={increment} />
